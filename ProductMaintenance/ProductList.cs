4a500d351f2e21f91fe73d433ed8e4a690bbecfb
anjuchawla <ANJU.CHAWLA@GEORGIANCOLLEGE.CA>
2017-12-21 12:01:31 -0500

    Creating ProductList class

---
+++ ProductMaintenance/ProductList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductMaintenance
+{
+    public class ProductList
+    {
+        //fields
+        private List<Product> products;
+
+        public delegate void ChangeHandler(ProductList products);//type definition  of a reference to a method
+        public event ChangeHandler Changed;  
+
+        /// <summary>
+        /// The constructor creates/instatntiates a List object to
+        /// store information of products in the inventory
+        /// </summary>
+        
+        public ProductList()
+        {
+            products = new List<Product>();
+        }
+
+        public int Count => products.Count;
+
+        //indexer
+
+        public Product this[int  i]
+        {
+            get
+            {
+                if ((i < 0) || (i >= products.Count))
+                    throw new ArgumentOutOfRangeException("Product does not exist at position " + (i + 1));
+                return products[i];
+            }
+            set
+            {
+                products[i] = value;
+                Changed(this);
+            }
+        }
+
+
+
+
+    }
+}
